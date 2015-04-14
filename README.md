74ae02319504fe07a2090b114344b1d2fb3e6b91
Carlos Abalde <carlos.abalde@gmail.com>
2015-04-14 15:43:29 +0200

    More photo sources + avoid downloading repeated wallpapers when using the --store opt

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-**ngwallpaper is a tiny Python script useful to programmatically download and decorate your OSX desktop with randomly selected wallpapers from the [collection provided by National Geographic](http://ngm.nationalgeographic.com/wallpaper).**
+**ngwallpaper is a tiny Python script useful to programmatically download and decorate your OSX desktop with randomly selected wallpapers provided by [National Geographic](http://www.nationalgeographic.com).** Supported repositories are:
+
+- [NGM latest wallpapers](http://ngm.nationalgeographic.com/wallpaper).
+- [NGM archived wallpapers](http://ngm.nationalgeographic.com/wallpaper/download).
+- Miscellaneous galleries: 'Life in Color' (i.e. ['kaleidoscope'](http://photography.nationalgeographic.com/photography/photos/life-color-kaleidoscope/), ['red'](http://photography.nationalgeographic.com/photography/photos/life-color-red/), ['orange'](http://photography.nationalgeographic.com/photography/photos/life-color-orange/), etc.), ['Visions of Earth'](http://photography.nationalgeographic.com/photography/photos/visions-of-earth-wallpapers), ['Bird Wallpapers'](http://animals.nationalgeographic.com/animals/photos/bird-wallpapers/), ['Underwater Wreckage'](http://photography.nationalgeographic.com/photography/photos/underwater-wrecks/), 'Patterns in Nature' (i.e. ['flora'](http://photography.nationalgeographic.com/photography/photos/patterns-flora/), ['reflections'](http://photography.nationalgeographic.com/photography/photos/patterns-nature-reflections/), etc.), etc.
 
 I created this script for my own personal use. It should work in older OSX versions, and it should be easily extended to support other platforms, multi-screen configurations, etc. Anyway, I haven't tested this in other platforms / configurations, and I don't plan to do that in the future :)
 
@@ -18,13 +22,13 @@ Usage
 
 3. Manually execute the script for the first time.
     ```
-    $ python2.7 ~/ngwallpaper/ngwallpaper.py --use-latest --use-archive --destination ~/Pictures/ngwallpapers/ --store --retries 5 --window 300
+    $ python2.7 ~/ngwallpaper/ngwallpaper.py --use-ngm-latest --use-ngm-archive --use-miscellaneous-galleries --destination ~/Pictures/ngwallpapers/ --store --retries 100
     ```
     This should change the background of your desktop and create a couple of files in `~/Pictures/ngwallpapers/`: the wallpaper image and a text file containing some information about the image.
 
 4. Add a new entry to your personal crontab (`crontab -e`) to periodically change your wallpaper.
     ```
-    0 * * * * /opt/local/bin/python2.7 ~/ngwallpaper/ngwallpaper.py --use-latest --use-archive --destination ~/Pictures/ngwallpapers/ --store --retries 5 --window 300
+    0 * * * * /opt/local/bin/python2.7 ~/ngwallpaper/ngwallpaper.py --use-ngm-latest --use-ngm-archive --use-miscellaneous-galleries --destination ~/Pictures/ngwallpapers/ --store --retries 100 > /dev/null
     ```
 
 5. When using the `--store`, previously downloaded wallpapers are not removed. You may want to use the folder containing all those images as a source for your screen saver.
--- ngwallpaper.py
+++ ngwallpaper.py
@@ -17,11 +17,11 @@ import sys
 import os
 import time
 import subprocess
-import traceback
 import argparse
 import urllib2
 import hashlib
 import re
+import glob
 import abc
 import random
 import tempfile
@@ -30,8 +30,52 @@ from xml.etree import ElementTree
 from BeautifulSoup import BeautifulSoup
 
 FILENAME = 'ngwallpaper'
+EXTENSIONS = ['.jpg', '.jpeg', '.png']
 
 HTTP_TIMEOUT = 30
+
+MISCELANEOUS_GALLERIES = [
+    # Life in Color.
+    'http://photography.nationalgeographic.com/photography/photos/life-color-kaleidoscope/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-red/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-orange/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-yellow/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-green/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-blue/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-purple/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-gold/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-silver/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-white/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-brown/',
+    'http://photography.nationalgeographic.com/photography/photos/life-color-black/',
+    # Patterns in Nature.
+    'http://photography.nationalgeographic.com/photography/photos/patterns-flora/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-nature-reflections/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-landscapes/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-nature-trees/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-animals/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-butterflies/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-nature-rainbows/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-island-aerials/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-snow-ice/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-rocks-lava/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-water/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-coral/',
+    'http://photography.nationalgeographic.com/photography/photos/patterns-aurorae/',
+    # Other.
+    'http://photography.nationalgeographic.com/photography/photos/visions-of-earth-wallpapers',
+    'http://animals.nationalgeographic.com/animals/photos/bird-wallpapers/',
+    'http://photography.nationalgeographic.com/photography/photos/underwater-wrecks/',
+    'http://photography.nationalgeographic.com/photography/photos/mysterious-earth/',
+    'http://photography.nationalgeographic.com/photography/photos/ocean-soul/',
+    'http://photography.nationalgeographic.com/photography/photos/extreme-earth/',
+    'http://photography.nationalgeographic.com/photography/photos/megatransect-gallery/',
+    'http://photography.nationalgeographic.com/photography/photos/cave-exploration/',
+    'http://photography.nationalgeographic.com/photography/photos/volcano-exploration/',
+    'http://photography.nationalgeographic.com/photography/photos/north-pole-expeditions/',
+    'http://adventure.nationalgeographic.com/adventure/everest/climbing-everest-photo-gallery/',
+    'http://adventure.nationalgeographic.com/adventure/mount-everest-photo-gallery',
+]
 
 
 class Origin(object):
@@ -50,6 +94,26 @@ class LeafOrigin(Origin):
 
     def __init__(self):
         super(LeafOrigin, self).__init__()
+
+    @property
+    def _timeout(self):
+        return HTTP_TIMEOUT
+
+    def _expand_href(self, url, href):
+        if href.startswith('http://') or href.startswith('https://'):
+            return href
+        elif href.startswith('//'):
+            return 'http:' + href
+        else:
+            parsed = urlparse.urlparse(url)
+            return parsed.scheme + '://' + parsed.netloc + href
+
+
+class NGMOrigin(LeafOrigin):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        super(NGMOrigin, self).__init__()
         self._cache = []
 
     @property
@@ -84,10 +148,6 @@ class LeafOrigin(Origin):
     def _root_url(self):
         return 'http://ngm.nationalgeographic.com'
 
-    @property
-    def _timeout(self):
-        return HTTP_TIMEOUT
-
     @abc.abstractproperty
     def _path(self):
         pass
@@ -105,9 +165,9 @@ class LeafOrigin(Origin):
         pass
 
 
-class Latest(LeafOrigin):
+class NGMLatest(NGMOrigin):
     def __init__(self):
-        super(Latest, self).__init__()
+        super(NGMLatest, self).__init__()
 
     @property
     def _path(self):
@@ -125,13 +185,13 @@ class Latest(LeafOrigin):
         result = []
         gallery = BeautifulSoup(contents).find('div', {'id': 'gallery'})
         for item in gallery.findAll('a', {'target': '_blank', 'href': re.compile(r'^/wallpaper/img/')}):
-            result.append(self._root_url + item['href'])
+            result.append(self._expand_href(self._root_url, item['href']))
         return result
 
 
-class Archive(LeafOrigin):
+class NGMArchive(NGMOrigin):
     def __init__(self):
-        super(Archive, self).__init__()
+        super(NGMArchive, self).__init__()
 
     @property
     def _path(self):
@@ -154,7 +214,52 @@ class Archive(LeafOrigin):
                 wallpaper.text.strip() or \
                 wallpaper[-1].text.strip()
             if url:
-                result.append(self._root_url + url)
+                result.append(self._expand_href(self._root_url, url))
+        return result
+
+
+class MiscellaneousGalleriesOrigin(LeafOrigin):
+    def __init__(self, urls):
+        super(MiscellaneousGalleriesOrigin, self).__init__()
+        self._urls = urls
+
+    @property
+    def photo(self):
+        # Initializations.
+        gallery_url = random.choice(self._urls)
+        wallpaper_url = None
+        result = None
+
+        # Select random wallpaper page URL from gallery.
+        fp = urllib2.urlopen(gallery_url, None, self._timeout)
+        if fp.getcode() == 200:
+            options = []
+            for item in BeautifulSoup(fp.read()).\
+                    find('div', {'id': 'gallery'}).\
+                    findAll('p', {'class': 'wallpaper_link'}):
+                options.append(item.find('a')['href'])
+            if len(options) > 0:
+                wallpaper_url = self._expand_href(
+                    gallery_url,
+                    random.choice(options))
+        fp.close()
+
+        # Fetch wallpaper page URL and extract image URL.
+        if wallpaper_url is not None:
+            fp = urllib2.urlopen(wallpaper_url, None, self._timeout)
+            if fp.getcode() == 200:
+                result = {
+                    'index': gallery_url,
+                    'url': self._expand_href(
+                        wallpaper_url,
+                        BeautifulSoup(fp.read()).
+                            find('div', {'id': 'content_top'}).
+                            find('div', {'class': 'download_link'}).
+                            find('a')['href']),
+                }
+            fp.close()
+
+        # Done!
         return result
 
 
@@ -173,18 +278,35 @@ class ComposedOrigin(Origin):
         return result
 
 
-def download_wallpaper(url, destination, filename):
-    # Extract extension & set destination file name.
-    file = os.path.join(
-        destination,
-        filename + os.path.splitext(urlparse.urlparse(url).path)[1])
+def wallpaper_exists(destination, filename):
+    for extension in EXTENSIONS:
+        if len(glob.glob(destination + filename + extension)) > 0:
+            return True
+    return False
 
-    # Download URL.
-    ifp = urllib2.urlopen(url, None, HTTP_TIMEOUT)
-    assert ifp.getcode() == 200
-    with open(file, 'wb') as ofd:
-        ofd.write(ifp.read())
-    ifp.close()
+
+def download_wallpaper(url, destination, filename):
+    # Initializations.
+    file = None
+
+    # Extract extension.
+    extension = os.path.splitext(urlparse.urlparse(url).path)[1].lower()
+
+    # Do not continue if the extension is not supported.
+    if extension in EXTENSIONS:
+        # Set destination file name.
+        file = os.path.join(
+            destination,
+            filename + extension)
+
+        # Download URL.
+        ifp = urllib2.urlopen(url, None, HTTP_TIMEOUT)
+        assert \
+            ifp.getcode() == 200, \
+            'Got unexpected HTTP response'
+        with open(file, 'wb') as ofd:
+            ofd.write(ifp.read())
+        ifp.close()
 
     # Done!
     return file
@@ -201,43 +323,80 @@ def script(code):
 
 def set_wallpaper(file):
     # See http://superuser.com/a/689804.
-    assert script('''
-        sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '%(file)s'"
-        killall Dock
-    ''' % {
-        'file': file,
-    }) == 0
+    assert \
+        script('''
+            sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '%(file)s'"
+            killall Dock
+        ''' % {
+            'file': file,
+        }) == 0, \
+        'Failed to set wallpaper'
 
 
 def main(origins, destination, store, retries):
-    # Do not continue if all reties have been exhausted.
-    if retries > 0:
+    # Initializations.
+    file = None
+
+    # Try to download a new wallpaper until all retries have been exhausted.
+    for i in xrange(retries, 0, -1):
+        # Initializations.
+        file = None
+
+        # Add some delay.
+        time.sleep(min(float((retries - i) * 100), 5000.0) / 1000.0)
+
+        # Ignore exceptions.
         try:
             # Fetch some random photo.
             wallpaper = ComposedOrigin(origins).photo
-            assert wallpaper is not None
+            assert \
+                wallpaper is not None, \
+                'Failed to fetch wallpaper'
 
             # Calculate destination filename.
             filename = FILENAME
             if store:
                 filename += '-' + hashlib.sha256(wallpaper['url']).hexdigest()
 
-            # Download selected photo.
-            file = download_wallpaper(wallpaper['url'], destination, filename)
-            assert file is not None
+            # Skip if the file already exists.
+            if not store or not wallpaper_exists(destination, filename):
+                # Download selected photo.
+                file = download_wallpaper(wallpaper['url'], destination, filename)
+                assert \
+                    file is not None, \
+                    'Failed to download wallpaper'
 
-            # Store meta data of the selected photo
-            with open(os.path.join(destination, filename + '.txt'), 'w') as fd:
-                fd.write(wallpaper['index'] + '\n')
-                fd.write(wallpaper['url'] + '\n')
+                # Store meta data of the selected photo
+                with open(os.path.join(destination, filename + '.txt'), 'w') as fd:
+                    fd.write(wallpaper['index'] + '\n')
+                    fd.write(wallpaper['url'] + '\n')
 
-            # Set the downloaded photo as wallpaper.
-            set_wallpaper(file)
-        except:
-            main(origins, destination, store, retries - 1)
+                # Done!
+                break
+            else:
+                sys.stdout.write('%(attempt)d: %(message)s\n' % {
+                    'attempt': i,
+                    'message': 'wallpaper already exists (%s)' % wallpaper['url'],
+                })
+        except Exception as e:
+            sys.stdout.write('%(attempt)d: %(message)s\n' % {
+                'attempt': i,
+                'message': 'unexpected failure (%s)' % e,
+            })
+
+    # If a new wallpaper was not downloaded, try to use an existing one.
+    if store and file is None:
+        downloaded = []
+        for extension in EXTENSIONS:
+            downloaded.extend(glob.glob(destination + FILENAME + '-*' + extension))
+        if len(downloaded) > 0:
+            file = random.choice(downloaded)
+
+    # Set the selected wallpaper.
+    if file is not None:
+        set_wallpaper(file)
     else:
-        sys.stderr.write('Failed to set wallpaper!\n')
-        traceback.print_exc()
+        sys.stderr.write('Failed to select a wallpaper!\n')
         sys.exit(1)
 
 
@@ -245,14 +404,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--use-latest', dest='origins', required=False,
-        action='append_const', const=Latest(),
-        help='enable "latest" repository')
+        '--use-ngm-latest', dest='origins', required=False,
+        action='append_const', const=NGMLatest(),
+        help='enable "NGM latest" repository')
 
     parser.add_argument(
-        '--use-archive', dest='origins', required=False,
-        action='append_const', const=Archive(),
-        help='enable "archive" repository')
+        '--use-ngm-archive', dest='origins', required=False,
+        action='append_const', const=NGMArchive(),
+        help='enable "NGM archive" repository')
+
+    parser.add_argument(
+        '--use-miscellaneous-galleries', dest='origins', required=False,
+        action='append_const', const=MiscellaneousGalleriesOrigin(MISCELANEOUS_GALLERIES),
+        help='enable "Miscellaneous galleries" repository')
 
     parser.add_argument(
         '--destination', dest='destination', type=str, required=False,
@@ -266,18 +430,12 @@ if __name__ == '__main__':
 
     parser.add_argument(
         '--retries', dest='retries', type=int, required=False,
-        default=5,
-        help='number of retries before failing')
-
-    parser.add_argument(
-        '--window', dest='window', type=int, required=False,
-        default=300,
-        help='fetch the same wallpaper even if the script is executed several times during the time window')
+        default=100,
+        help='number of retries before failing / using a previously downloaded wallpaper')
 
     options = parser.parse_args()
 
-    if options.origins and options.retries > 0 and options.window > 0:
-        random.seed(int(time.time() / options.window))
+    if options.origins and options.retries > 0:
         main(options.origins, options.destination, options.store, options.retries)
     else:
         parser.print_help()
